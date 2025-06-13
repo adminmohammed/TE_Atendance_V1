a5c751c05497a5597c199035ba6eac4a89088fc5
adminmohammed <105809983+adminmohammed@users.noreply.github.com>
2025-06-13 18:44:43 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "bodacious"
+games { 'gta5' }
+lua54 "yes"
+
+name 'TE_Atendance_V1'
+version 'v1.0.1'
+author 'Al-Dolime'
+
+server_scripts {
+	"@vrp/lib/utils.lua",
+    "File/ServerSide.lua",
+    "File/Server.lua",
+    "config.lua"
+}
+
+
+
+
