a2a196606c231a407699915bfaa0752f0f4e515b
adminmohammed <105809983+adminmohammed@users.noreply.github.com>
2025-06-13 18:48:32 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,12 +6,22 @@ name 'TE_Atendance_V1'
 version 'v1.0.1'
 author 'Al-Dolime'
 
+
+escrow_ignore {
+  "config.lua",
+}
+
+
 server_scripts {
 	"@vrp/lib/utils.lua",
     "File/ServerSide.lua",
     "File/Server.lua",
     "config.lua"
 }
+
+
+
+
 
 
 
